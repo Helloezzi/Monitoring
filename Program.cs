c2582f039f9bdc8e977af908c6bf8cee19dda237
hwjung <hwjung@zinnotech.com>
2020-03-03 14:14:27 +0900

    modify

--- Program.cs
+++ Program.cs
@@ -7,16 +7,38 @@ namespace Monitoring
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Press ENTER to start the timer");
             Console.ReadLine();
             Recorder.Start();
 
             int[] largeArrayOfInts = Enumerable.Range(1, 10000).ToArray();
 
-            //Console.Write($"count{largeArrayOfInts.Length}");
-
             Console.Write("Press ENTER to stop the timer");
             Console.ReadLine();
+            Recorder.Stop();
+            Console.ReadLine();
+            */
+
+            int[] numbers = Enumerable.Range(1, 10000).ToArray();
+            Recorder.Start();
+            Console.WriteLine("using string");
+
+            string s = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                s += numbers[i] + ", ";                
+            }
+            Recorder.Stop();
+
+            Recorder.Start();
+            Console.WriteLine("using StringBuilder");
+            var builder = new System.Text.StringBuilder();
+            for (int i= 0; i < numbers.Length; i++)
+            {
+                builder.Append(numbers[i]);
+                builder.Append(", ");
+            }
             Recorder.Stop();
             Console.ReadLine();
         }
