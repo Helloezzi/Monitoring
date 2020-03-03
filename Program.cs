afcc767426ebe5805d78c2ef1c2e1f19b6f157d3
hwjung <hwjung@zinnotech.com>
2020-03-03 13:16:16 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Monitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press ENTER to start the timer");
+            Console.ReadLine();
+            Recorder.Start();
+
+            int[] largeArrayOfInts = Enumerable.Range(1, 10000).ToArray();
+
+            //Console.Write($"count{largeArrayOfInts.Length}");
+
+            Console.Write("Press ENTER to stop the timer");
+            Console.ReadLine();
+            Recorder.Stop();
+            Console.ReadLine();
+        }
+    }
+}
